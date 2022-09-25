f45f96008091a38a8238da60a0b69064b50ef35b
Meraat <Meraat@gmail.com>
2022-09-25 21:22:14 +0330

    made result page by searching by user

--- src/components/Sura.jsx
+++ src/components/Sura.jsx
@@ -1,11 +1,87 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { QuranDataPage, QuranDataSura } from '../quran-resources (farawin)/quran-metadata'
 
 const Sura = () => {
-  return (
+  const pages = QuranDataPage
+  const data = QuranDataSura
+  const navigate = useNavigate()
+  const [isEmpty, setIsEmpty] = useState(true)
+  let temp = []
+  const [foundSuras, setFoundSuras] = useState([])
 
-    <div>Sura</div>
+
+  const findSura = (index) => {
+
+  }
+
+  const findMatches = (e) => {
+    if (e.target.value !== '') {
+      setIsEmpty(false)
+    }
+    temp = []
+    for (let c = 0; c < data.length; c++) {
+      if
+        (
+        data[c][4].includes(e.target.value) ||
+        data[c][5].toLowerCase().includes(e.target.value.toLowerCase()) ||
+        data[c][6].toLowerCase().includes(e.target.value.toLowerCase())
+      ) {
+        temp.push(data[c])
+      }
+    }
+    setFoundSuras(temp)
+  }
+
+
+
+  return (
+    <div className='wrapper-sura'>
+
+      <div className='sura-search-all'>
+        <div className='sura-search-area relative'>
+          <input
+            type="text"
+            placeholder='نام سوره'
+            className='sura-search-input'
+            onChange={findMatches}
+          />
+          <i
+            className="sura-search-icon fa-sharp fa-solid fa-magnifying-glass">
+          </i>
+        </div>
+      </div>
+
+      <div className='all-suras'>
+        {isEmpty ?
+          data.map((sura, index) => {
+            return (<div
+              key={index}
+              className='sura'
+              onClick={() => { findSura(index) }}>
+              <span>{index + 1}</span>
+              <p className='persian-name'>{sura[4]}</p>
+              <p>{sura[6]}</p>
+              <p>تعداد آیات: {sura[1]} </p>
+              <p>{sura[7]}</p>
+            </div>)
+          }) :
+          foundSuras.map((sura, index) => {
+            return (<div
+              key={index}
+              className='sura'
+              onClick={() => { findSura(index) }}>
+              <span>{index + 1}</span>
+              <p className='persian-name'>{sura[4]}</p>
+              <p>{sura[6]}</p>
+              <p>تعداد آیات: {sura[1]} </p>
+              <p>{sura[7]}</p>
+            </div>)
+          })
+        }
+      </div>
+
+    </div>
   )
 }
-
 export default Sura
